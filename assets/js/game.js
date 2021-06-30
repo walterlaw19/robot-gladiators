3a3882e1289af0724501749776749755d8a101e8
SC2021 <walterlaw19@gmail.com>
2021-06-29 23:10:19 -0400

    add prompt and console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("this is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name?");
+// what is this?
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message")
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+// this creates a function named "fight"  //
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
